60741fb9e3626f6074f92e1a311b07277dcd1090
Yumi Izumi <me@yumiizumi.com>
2023-10-02 08:40:22 -0400

    Add plist config

--- Formula/xcode-discord-rpc.rb
+++ Formula/xcode-discord-rpc.rb
@@ -8,4 +8,25 @@ class XcodeDiscordRpc < Formula
   def install
     bin.install "xcode-discord-rpc"
   end
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{bin}/xcode-discord-rpc</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+      </dict>
+      </plist>
+    EOS
+  end
 end
