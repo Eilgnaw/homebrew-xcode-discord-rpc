2bf5592000a574824b3d851b7469bcdeca0145f8
Yumi Izumi <me@yumiizumi.com>
2023-10-02 10:37:42 -0400

    Release v0.1.1

--- Formula/xcode-discord-rpc.rb
+++ Formula/xcode-discord-rpc.rb
@@ -1,9 +1,9 @@
 class XcodeDiscordRpc < Formula
   desc "A simple Discord Rich Presence client for Xcode"
   homepage "https://github.com/izyumidev/xcode-discord-rpc"
-  url "https://github.com/izyumidev/xcode-discord-rpc/releases/download/v0.1.0/xcode-discord-rpc.tar.gz"
-  sha256 "86869c5b3163768b3bcb7961fdd100407b8a9258de7d73846a5cdfd2882835be"
-  version "0.1.0"
+  url "https://github.com/izyumidev/xcode-discord-rpc/releases/download/v0.1.1/xcode-discord-rpc.tar.gz"
+  sha256 "ddfc8ce2f5ff7be84dcbb4ac229c69d74c0b6ad54a392b42b95a566d0864f042"
+  version "0.1.1"
 
   def install
     bin.install "xcode-discord-rpc"
