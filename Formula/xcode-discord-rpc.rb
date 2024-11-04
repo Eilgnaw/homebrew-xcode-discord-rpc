fe86491679bd8cbdcc08a4246b391899e3552a49
Eilgnaw <710455571@qq.com>
2024-11-04 18:30:57 +0800

    Release v0.2.5

--- Formula/xcode-discord-rpc.rb
+++ Formula/xcode-discord-rpc.rb
@@ -1,9 +1,9 @@
 class XcodeDiscordRpc < Formula
   desc "A simple Discord Rich Presence client for Xcode"
   homepage "https://github.com/izyumidev/xcode-discord-rpc"
-  url "https://github.com/Eilgnaw/xcode-discord-rpc/releases/download/v0.2.4/xcode-discord-rpc.tar.gz"
-  sha256 "22275f8a8d7d08ff993b3458d3755fc8a25f969c5d9aa7229e43c25ce5df63a7"
-  version "0.2.4"
+  url "https://github.com/Eilgnaw/xcode-discord-rpc/releases/download/v0.2.5/xcode-discord-rpc.tar.gz"
+  sha256 "af45697f3254e19bf3e3261ee4156fb1ded493407aad68cdd5d92542ea900d0f"
+  version "0.2.5"
 
   def install
     bin.install "xcode-discord-rpc"
